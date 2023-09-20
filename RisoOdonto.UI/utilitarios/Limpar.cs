3709b57287885e6fc3de5dc5fb7f3bf9808d1f8a
Bruno Silva <silvabrunin503@gmail.com>
2023-09-20 17:03:16 -0300

    pagina de Login principal, sem pagina de Funcionarios e classe Limpar ok

--- RisoOdonto.UI/utilitarios/Limpar.cs
+++ RisoOdonto.UI/utilitarios/Limpar.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
+using System.Web.UI;
 
 namespace RisoOdonto.UI.utilitarios
 {
     public class Limpar
     {
+        public static void ClearControl(Control ctrl)
+        {
+            foreach (Control c in ctrl.Controls)
+            {
+                if (c is TextBox)
+                {
+                    ((TextBox)c).Text = string.Empty;
+                }
+                else if (c is Label)
+                {
+                    ((Label)c).Text = string.Empty;
+                }
+                else if (c is DropDownList)
+                {
+                    ((DropDownList)c).ClearSelection();
+                }
+                else if (c is RadioButtonList)
+                {
+                    ((RadioButtonList)c).ClearSelection();
+                }
+                ClearControl(c);
+
+            }
+        }
     }
 }
