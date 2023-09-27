c97083d79220d97ca3ca1acf4d2aa0d2ba4ef5b6
Kelvin Bryan <k.bryan924@gmail.com>
2023-09-26 22:17:09 -0300

    terminando agendamentoDAL.

--- RisoOdonto.DAL/AgendamentoDAL.cs
+++ RisoOdonto.DAL/AgendamentoDAL.cs
@@ -18,7 +18,7 @@ namespace RisoOdonto.DAL
             try
             {
                 Conectar();
-                cmd = new SqlCommand("SELECT IdAgendamento,IdPaciente,IdDentista,DataConsulta,Horario,PrimeiraConsulta,StatusConsulta FROM Agendamento INNER JOIN TipoUsuario ON IdTipoUsuario = TpUsuario ORDER BY IdAgendamento ASC;", conn);
+                cmd = new SqlCommand("SELECT IdAgendamento, IdPaciente, IdDentista, DataConsulta, Horario, PrimeiraConsulta, StatusConsulta FROM Agendamento INNER JOIN IdAgendamento ON IdPaciente = Paciente ORDER BY IdAgendamento ASC INNER JOIN IdDentista;", conn);
                 dr = cmd.ExecuteReader();
                 List<AgendamentoDTO> lista = new List<AgendamentoDTO>();
                 while (dr.Read())
@@ -27,8 +27,8 @@ namespace RisoOdonto.DAL
                     obj.IdAgendamento = Convert.ToInt32(dr["IdAgendamento"]);
                     obj.IdPaciente = Convert.ToInt32(dr["IdPaciente"]);
                     obj.IdDentista = Convert.ToInt32(dr["IdDentista"]);
-                    obj.DataConsulta = Convert.ToDateTime(dr["DataNasciemento"]);
-                    obj.Horario = Convert.ToDateTime(dr["DataNasciemento"]);
+                    obj.DataConsulta = Convert.ToDateTime(dr["DataConsulta"]);
+                    obj.Horario = Convert.ToDateTime(dr["Horario"]);
                     obj.PrimeiraConsulta = Convert.ToBoolean(dr["PrimeiraConsulta"]);
                     obj.StatusConsulta = dr["StatusConsulta"].ToString();
                     lista.Add(obj);
